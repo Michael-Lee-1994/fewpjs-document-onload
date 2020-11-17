66a38845301a5d8e8c18d8fad6f9b430ca0b7a48
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2020-11-16 17:01:19 -0800

    commit tree 8c6b9cc838e976ebe93a7218ac30d9f06bc19010

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    updateDOM();
+  });
+  
+function updateDOM() {
+    console.log("Change DOM");
+    document.querySelector("p#text").innerHTML = "This is really cool!";
+}
